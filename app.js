42bdf2514ac8395cc8499dc6d050a881a964fc87
Yummytanmo <147129696+Yummytanmo@users.noreply.github.com>
2025-05-27 15:24:43 +0800

    Can run on phone

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // app.js
 App({
   onLaunch() {
+    console.log('小程序启动')
+    
+    // 获取系统信息
+    wx.getSystemInfo({
+      success: (res) => {
+        console.log('系统信息：', res)
+      }
+    })
+
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
@@ -13,6 +22,15 @@ App({
       }
     })
   },
+  
+  onShow() {
+    console.log('小程序显示')
+  },
+  
+  onHide() {
+    console.log('小程序隐藏')
+  },
+  
   globalData: {
     userInfo: null
   }
--- pages/home/home.js
+++ pages/home/home.js
@@ -5,70 +5,63 @@ Page({
         id: 1,
         title: '今日热量',
         value: '0',
-        unit: 'kcal',
-        icon: '/assets/icons/calorie.png'
+        unit: 'kcal'
       },
       {
         id: 2,
         title: '蛋白质',
         value: '0',
-        unit: 'g',
-        icon: '/assets/icons/protein.png'
+        unit: 'g'
       },
       {
         id: 3,
         title: '碳水化合物',
         value: '0',
-        unit: 'g',
-        icon: '/assets/icons/carbs.png'
+        unit: 'g'
       },
       {
         id: 4,
         title: '脂肪',
         value: '0',
-        unit: 'g',
-        icon: '/assets/icons/fat.png'
+        unit: 'g'
       },
       {
         id: 5,
         title: '维生素',
         value: '0',
-        unit: '%',
-        icon: '/assets/icons/vitamin.png'
+        unit: '%'
       },
       {
         id: 6,
         title: '膳食纤维',
         value: '0',
-        unit: 'g',
-        icon: '/assets/icons/fiber.png'
+        unit: 'g'
       }
     ]
   },
 
   onLoad() {
+    console.log('页面加载完成')
     this.loadHealthMetrics()
   },
 
   loadHealthMetrics() {
-    // TODO: 从后端API获取健康指标数据
-    // wx.request({
-    //   url: 'your-api-endpoint',
-    //   success: (res) => {
-    //     this.setData({
-    //       healthMetrics: res.data
-    //     })
-    //   }
-    // })
+    console.log('加载健康指标数据')
+    // 先使用静态数据
+    this.setData({
+      healthMetrics: this.data.healthMetrics
+    })
   },
 
   goToAnalyze() {
+    console.log('跳转到分析页面')
     wx.switchTab({
       url: '/pages/analyze/analyze'
     })
   },
 
   goToProfile() {
+    console.log('跳转到个人中心')
     wx.switchTab({
       url: '/pages/profile/profile'
     })
